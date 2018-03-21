db12b48122c3c686986bd752923dad111575aa81
Matthew A. Layton <matthew.layton@future-skies.com>
2018-03-21 16:38:21 -0400

    Added comments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
-
+// A simple class to test out EGit
 public class HelloWorld {
+	// The main routine
 	public static void main(String[] args) {
+		// Just print something to show that it works
 		System.out.println("Hello World!");
 	}
 }
