d099d121d31d4e055eb46a02a0a38d63a17fc6ad
Matthew A. Layton <matthew.layton@future-skies.com>
2018-03-21 17:58:10 -0400

    Updated for demonstration
    Update to have the work done in a private method that main calls after
    instantiation

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,13 @@
 public class HelloWorld {
 	// The main routine
 	public static void main(String[] args) {
+		// Construct an instance of the class
+		HelloWorld hw = new HelloWorld();
+		// Invoke a private method that does what we want to be done
+		hw.doIt();
+	}
+	
+	private void doIt() {
 		// Just print something to show that it works
 		System.out.println("Hello World!");
 	}
